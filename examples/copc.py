97f8297e8037aa4a112cf4f8a67ebf3d63a2b1b4
Aitor Arjona <aitor.a98@gmail.com>
2022-09-21 23:07:59 +0200

    Multi query test

--- examples/copc.py
+++ examples/copc.py
@@ -2,6 +2,9 @@ from laspy import CopcReader, Bounds
 import time
 import laspy
 
+from laspy.copc import load_octree_for_query
+from operator import attrgetter
+
 def create_query(header):
     querys = []
 
@@ -26,31 +29,59 @@ def create_query(header):
 
     return querys
 
+
+def multi_query(header, bounds_list):
+
+
+    pass
+
 def main():
-    # path = "http://localhost:8000/autzen-classified.copc(1).laz"
-    # path = "http://localhost:8000/sofi.copc.laz"
-    # path = "https://s3.amazonaws.com/hobu-lidar/autzen-classified.copc.laz"
-    # path = "https://s3.amazonaws.com/hobu-lidar/sofi.copc.laz"
-    # path = "https://s3.amazonaws.com/hobu-lidar/montreal-2015.copc.laz"
-    path = "https://s3.amazonaws.com/hobu-lidar/melbourne-2018.copc.laz"
+    path = "http://192.168.1.110:9000/geospatial/copc/lidar_sample.copc.laz"
     
     with CopcReader.open(path) as crdr:
         print("copc_reader_ready")
-        # querys = create_query(crdr.header)
+        querys = create_query(crdr.header)
+
+        # points = crdr.query(querys[0], resolution=None)
+
+        for query in querys:
+            query = query.ensure_3d(crdr.header.mins, crdr.header.maxs)
+            t0 = time.time()
+            octree = load_octree_for_query(crdr.source, crdr.copc_info, crdr.root_page, query, None)
+
+            nodes_to_read = sorted(octree, key=attrgetter("offset"))
+            grouped_nodes = []
+            current_group = []
+            last_node_end = nodes_to_read[0].offset
+            for node in nodes_to_read:
+                if node.offset == last_node_end:
+                    current_group.append(node)
+                    last_node_end += node.byte_size
+                else:
+                    grouped_nodes.append(current_group)
+                    current_group = [node]
+                    last_node_end = node.offset + node.byte_size
+            if current_group:
+                grouped_nodes.append(current_group)
+            t1 = time.time()
+            print(t1 - t0)
+
+        # multi_query(crdr.header, querys)
+
         # for i, query_bounds in enumerate(querys):
         #     resolution = None
         #     points = crdr.query(query_bounds, resolution=resolution)
         #     print("Spatial Query gave:", points)
         #     print(len(points) / crdr.header.point_count * 100);
 
-            # new_header = laspy.LasHeader(
-            #     version=crdr.header.version,
-            #     point_format=crdr.header.point_format
-            # )
-            # new_header.offsets = crdr.header.offsets
-            # new_header.scales = crdr.header.scales
-            # with laspy.open(f"output_{i}.las", mode="w", header=new_header) as f:
-            #     f.write_points(points)
+        #     new_header = laspy.LasHeader(
+        #         version=crdr.header.version,
+        #         point_format=crdr.header.point_format
+        #     )
+        #     new_header.offsets = crdr.header.offsets
+        #     new_header.scales = crdr.header.scales
+        #     with laspy.open(f"output_{i}.las", mode="w", header=new_header) as f:
+        #         f.write_points(points)
 
 
 
